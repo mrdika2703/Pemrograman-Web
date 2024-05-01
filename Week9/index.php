dae0e613b90b9eb8123441348a59595a0f382faf
Wahyu Adam Anandika <wahyuadamanandika01@gmail.com>
2024-05-01 21:22:42 +0700

    Dika

---
+++ Week9/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Praktikum 7</title>
+    <style>
+        div {
+            margin: 15px;
+        }
+
+        a {
+            background-color: #008CBA;
+            border: 1px solid blue;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            cursor: pointer;
+            float: left;   
+        }
+
+        a:hover {
+            background-color: #129ecc;
+        }
+
+    </style>
+</head>
+<body>
+    <div>
+        <a href="htmlPhp.php">HTML PHP</a>
+        <a href="basic.php">Basic</a>
+        <a href="variabelConst.php">Variabel Const</a>
+        <a href="dataType.php">Data Type</a>
+        <a href="operator.php">Operator</a>
+        <a href="controlStructure.php">Control Structure</a>
+        <a href="userDefined.php">User Defined</a>
+        <a href="phpFunctuin.php">PHP Function</a>
+    </div>
+</body>
+</html>
